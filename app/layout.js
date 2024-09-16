9c9215bba7e4a033f6cd2c131b80978cc1644413
TaylorTan1998 <haoyi2@hotmail.com>
2024-09-17 00:21:08 +0800

    added meta data

--- app/layout.js
+++ app/layout.js
@@ -18,6 +18,9 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className={`${sfprobold.variable}`}>
+      <head>
+        <meta name="google-site-verification" content="9YqE7E_anQFzzFlDlZWg00R6asvYwKL0toFtZ1-U5o0" />
+      </head>
       <body className={`${sfpro.className}`}>
         {children}
       </body>
