98a9ca2c3c4021364908cafff4f43cf808d9a84f
TaylorTan1998 <haoyi2@hotmail.com>
2024-09-18 01:07:54 +0800

    change visual

--- app/projects/[id]/page.js
+++ app/projects/[id]/page.js
@@ -3,7 +3,6 @@ import React from "react";
 import { useParams } from 'next/navigation';
 import Navbar from "../../../app/components/Navbar";
 import ProjectDetail from "../../../app/components/ProjectPage";
-import ProjectSection from "../../../app/components/ProjectSection";
 import EmailSection from "../../../app/components/EmailSection";
 import Footer from "../../../app/components/Footer";
 import ProjectsSection from "../../../app/components/ProjectSection";
@@ -12,7 +11,7 @@ const ProjectPage = () => {
     const { id } = useParams();
 
     return (
-        <main className="flex min-h-screen flex-col bg-white">
+        <main className="flex min-h-screen flex-col bg-gradient-to-b from-blue-200 to-blue-50">
             <Navbar />
             <div className='container mt-24 mx-auto px-12 py-4'>
                 <ProjectDetail projectId={id} />
